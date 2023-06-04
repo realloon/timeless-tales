00b58baff5d9db88ab369ff22822fe7903d75256
李真 <sincerajn@outlook.com>
2023-06-04 21:42:09 +0800

    add footer component

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,19 +1,29 @@
-// 
+import Button from 'Components/Button'
+//
+import github from 'icons/github.svg'
+//
 import './index.css'
 
 export default function Footer({ title = 'Page Title', name = 'name' }) {
   return (
-    <footer className='footer'>
-      <section className='copyright'>
+    <footer className="footer">
+      <section className="about">
+        <nav>
+          <Button
+            type="icon"
+            content="Github"
+            img={github}
+            handleClick={() => {
+              window.open('https://github.com/realloon/timeless-tales')
+            }}
+          ></Button>
+        </nav>
+      </section>
+      <section className="copyright">
         <h2>{title}</h2>
         <p>
           Copyright © {new Date().getFullYear()} {name}. All rights reserved.
         </p>
-      </section>
-      <section>
-        <nav>
-          Github
-        </nav>
       </section>
     </footer>
   )
